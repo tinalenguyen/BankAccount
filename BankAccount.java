82f800d2d5b09e2e4379abdaf83d2d61bb0c568e
Tina Nguyen <thetinalenguyen@gmail.com>
2020-10-26 23:35:07 -0400

    Added a constructor

---
+++ BankAccount.java
@@ -0,0 +1,20 @@
+public class BankAccount {
+
+//instance variables
+private double balance;
+private int accountID;
+private String password;
+
+//constructor
+
+public BankAccount (int accountnum, String pass){
+
+ accountID = accountnum;
+ password = pass;
+ balance = 0;
+
+}
+
+
+
+}
